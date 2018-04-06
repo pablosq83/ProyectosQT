aaff1bef147c9c81ffcad35f6a3d91bfded12184
Pablo Sulbaran <psulbaran@cenditel.gob.ve>
2018-04-06 11:43:59 -0400

    Ejemplo 2. Interfaz gráfica que permite crear una carpeta

--- Ejemplo 2/Ejemplo2/mainwindow.cpp
+++ Ejemplo 2/Ejemplo2/mainwindow.cpp
@@ -12,3 +12,12 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString texto = ui->lineEdit->text();
+    QString comando = "mkdir "+texto;
+    system(comando.toUtf8().constData());
+    ui->label->clear();
+    ui->label->setText("Carpeta creada");
+}
